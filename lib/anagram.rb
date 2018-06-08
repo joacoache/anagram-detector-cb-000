4d7f1c729b2332ec2ccc4d116c8e93a29e4b1251
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-08 00:04:13 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  @@array = []
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.each do |words|
+      words.each do |letters|
+        a = letters.split("").sort
+        if a == @word.split("").sort
+          @@array << letters
+        end
+      end
+    end      
+
+end
